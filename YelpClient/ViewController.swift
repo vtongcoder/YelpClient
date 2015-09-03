a81479c6426b2e1d3fe91efc41bab76676d1185f
Dan Tong <tongvtdan@outlook.com>
2015-09-03 11:20:31 +0700

    Initial Commit

---
+++ YelpClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YelpClient
+//
+//  Created by Dan Tong on 9/3/15.
+//  Copyright © 2015 DanTong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
